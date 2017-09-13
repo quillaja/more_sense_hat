f4d3955bfc91c84e945bced59a50658a8db4e02a
ben <quillaja@hotmail.com>
2017-09-13 01:08:40 -0700

    added FakeHat() to fun.py. It can (sorta) simulate a SenseHat's LEDs by producing images with PIL every time set_pixels() is called.

--- fun.py
+++ fun.py
@@ -1,11 +1,36 @@
 import time
 import math
+from PIL import Image
 
 import progress_bar as pg
+import palettes as p
 
 
 def show_cos(prog_bar, max, revs=2, delay=0.05):
-	for r in (2*math.pi*(i/100.0) for i in range(int(revs*100)+1)):
-		_ = prog_bar(-max*math.cos(r))
-		time.sleep(delay)
+    for r in (2 * math.pi * (i / 100.0) for i in range(int(revs * 100) + 1)):
+        _ = prog_bar(-max * math.cos(r))
+        time.sleep(delay)
 
+
+class FakeHat:
+    """Implements an interface similar to `SenseHat` but displays the LED
+    matrix as an image using PIL. Only implements methods used in this
+    library. Image displaying is slow and crappy, but works for testing."""
+
+    def __init__(self, data=None):
+        if data:
+            self.data = data
+        else:
+            self.clear()
+
+    def clear(self):
+        self.data = [p.OFF] * 64
+
+    def set_pixels(self, data):
+        self.data = data
+        im = Image.new('RGB', (8, 8))
+        im.putdata(data)
+        im.show()
+
+    def get_pixels(self):
+        return self.data
